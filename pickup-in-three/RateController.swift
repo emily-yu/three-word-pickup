ce648e831aade30945e275978fe436e19fc3f588
ey1031 <eyudeveloper@gmail.com>
2017-07-14 18:45:46 -0700

    Importing post data from Firebase

--- pickup-in-three/RateController.swift
+++ pickup-in-three/RateController.swift
@@ -15,24 +15,60 @@ class RateController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var ref: FIRDatabaseReference!
     
+    // line data
+    var lineText = [String]()
+    var lineLike = [Int]()
+    var lineUser = [String]()
+    var lineKey = [String]()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        ref = FIRDatabase.database().reference()
-        self.loadData()
-//        print("loaded")
         // Do any additional setup after loading the view, typically from a nib.
+        ref = FIRDatabase.database().reference()
+        super.viewDidLoad()
+        self.loadData()
     }
     
     func loadData() {
-        print("asdjklfajslkfjaslfkjlsadjfklasd")
-//        ref = FIRDatabase.database().reference()
-        ref.observe(.value, with: {
-            snapshot in
-            var restaurantNames = [String]()
-            for restaurant in snapshot.children {
-                restaurantNames.append((restaurant as AnyObject).key)
+        
+        let likeRef = ref.child("lines")
+        likeRef.observe(.value, with: { snapshot in
+            
+            for line in snapshot.children {
+                self.lineText.append((line as AnyObject).key)
             }
-            print(restaurantNames)
+            let count = Int(snapshot.childrenCount)
+            print("COUNT:\(count)")
+            
+            // line is the key to navigate through
+            for lines in self.lineText {
+                
+                // likes on each line
+                likeRef.child(lines).child("likes").observe(.value, with: {
+                    snapshot in
+                    self.lineLike.append(snapshot.value as! Int)
+                })
+                
+                // user on each line
+                likeRef.child(lines).child("username").observe(.value, with: {
+                    snapshot in
+                    self.lineUser.append(snapshot.value as! String)
+                })
+                
+                // user on each line
+                likeRef.child(lines).child("keys").observe(.value, with: {
+                    snapshot in
+                    for line in snapshot.children {
+                        self.lineKey.append((line as AnyObject).value)
+                    }
+                    if (self.lineLike.count == count) {
+                        // reload data
+                        print(self.lineText)
+                        print(self.lineLike)
+                        print(self.lineUser)
+                        print(self.lineKey)
+                    }
+                })
+            }
         })
     }
 

fbd7a96ab81f54e605c2e5152feb424cb4098d36
ey1031 <eyudeveloper@gmail.com>
2017-09-03 23:44:53 -0700

    changing some ui and messing around

--- pickup-in-three/CreateController.swift
+++ pickup-in-three/CreateController.swift
@@ -13,53 +13,60 @@ import SwiftyJSON
 
 class CreateController: UIViewController {
 
-    @IBOutlet var textField: UITextView!
+    @IBOutlet var scrollView: UIScrollView!
     
+    // Random Word creation
+    @IBOutlet var textField: UITextView!
     @IBAction func refresh(_ sender: Any) {
         refreshAll();
     }
-    
     @IBOutlet var word1: UIButton!
     @IBAction func word1(_ sender: Any) {
         self.textField.text = "\(textField.text!) \(self.word1.currentTitle!)";
     }
-    
     @IBOutlet var word2: UIButton!
     @IBAction func word2(_ sender: Any) {
         self.textField.text = "\(textField.text!) \(self.word2.currentTitle!)";
     }
-    
     @IBOutlet var word3: UIButton!
     @IBAction func word3(_ sender: Any) {
-        // append word to textfield.content
         self.textField.text = "\(textField.text!) \(self.word3.currentTitle!)";
-        print(word3.currentTitle!)
     }
-    
-    @IBAction func nextButton(_ sender: Any) {
-        // retrieve three random words
-        // replace word1.content, word2.content, word3.content
-    }
-    
     @IBAction func submitButton(_ sender: Any) {
-        if  (textField.text.lowercased().range(of: (word3.currentTitle?.lowercased())!) != nil) &&
-            (textField.text.lowercased().range(of: (word3.currentTitle?.lowercased())!) != nil) &&
+        if  (textField.text.lowercased().range(of: (word1.currentTitle?.lowercased())!) != nil) &&
+            (textField.text.lowercased().range(of: (word2.currentTitle?.lowercased())!) != nil) &&
             (textField.text.lowercased().range(of: (word3.currentTitle?.lowercased())!) != nil) {
-            
-            // Generate new set of words
-            refreshAll();
-            
-            print("TODO: Submit content to Firebase")
+            refreshAll(); // Generate new set of words
             // appFunctions().incrementPoints();
+            print("TODO: Submit to Firebase");
         }
         else {
             let alertController = UIAlertController(title: "Error", message: "Please use the above words to create your pickup line.", preferredStyle: .alert);
-            
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil);
             alertController.addAction(defaultAction);
-            
             present(alertController, animated: true, completion: nil);
         }
+    }
+    
+    // Brainstorm creation
+    @IBOutlet var keyword1: UITextField!
+    @IBOutlet var keyword2: UITextField!
+    @IBOutlet var keyword3: UITextField!
+    @IBOutlet var createTextField: UITextView!
+    @IBAction func createSubmitButton(_ sender: Any) {
+        if  (keyword1.text != "") &&
+            (keyword2.text != "") &&
+            (keyword3.text != "") &&
+            (createTextField.text.lowercased().range(of: keyword1.text!.lowercased()) != nil) &&
+            (createTextField.text.lowercased().range(of: keyword2.text!.lowercased()) != nil) &&
+            (createTextField.text.lowercased().range(of: keyword3.text!.lowercased()) != nil) {
+            print("TODO: Submit to Firebase");
+        }
+    }
+    @IBAction func clearFields(_ sender: Any) {
+        keyword1.text = "";
+        keyword2.text = "";
+        keyword3.text = "";
     }
     
     // Generate new set of words
@@ -71,6 +78,7 @@ class CreateController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height+100)
         refreshAll();
     }
 

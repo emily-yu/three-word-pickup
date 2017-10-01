f1bc430270d33e7e2bcbf9d6a16dbfeca3cbb4ec
ey1031 <eyudeveloper@gmail.com>
2017-09-30 23:58:19 -0700

    adding responding to keyword requests

--- pickup-in-three/RequestController.swift
+++ pickup-in-three/RequestController.swift
@@ -55,10 +55,57 @@ class RequestController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "Create", message: "The required words are: \(requests[indexPath.row][0]), \(requests[indexPath.row][1]), and \(requests[indexPath.row][2]).", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+        }
+
+        let defaultAction = UIAlertAction(title: "Submit", style: .cancel, handler: { (_) in
+            let textField = alertController.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(textField.text)")
+
+            if  (alertController.textFields![0].text?.range(of: self.requests[indexPath.row][0].lowercased()) != nil) &&
+                (alertController.textFields![0].text?.range(of: self.requests[indexPath.row][1].lowercased()) != nil) &&
+                (alertController.textFields![0].text?.range(of: self.requests[indexPath.row][2].lowercased()) != nil) {
+                
+                self.ref.child("lines").child(alertController.textFields![0].text!).setValue([
+                    "keys": [
+                        "1" : "\(self.requests[indexPath.row][0])",
+                        "2" : "\(self.requests[indexPath.row][1])",
+                        "3" : "\(self.requests[indexPath.row][2])",
+                    ],
+                    "likes" : 0,
+                    "username" : FIRAuth.auth()!.currentUser!.uid,
+                ] as NSDictionary);
+                
+                // TODO: Remove request after x pickup lines submitted
+            }
+            else {
+                let alertController = UIAlertController(title: "Error", message: "Required words were not present.", preferredStyle: .alert);
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil);
+                alertController.addAction(defaultAction);
+                self.present(alertController, animated: true, completion: nil);
+            }
+        });
+        
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
         tableView.deselectRow(at: indexPath, animated: true)
     }
     // tableView -- END MARKER
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Placeholder"
+            textView.textColor = UIColor.lightGray
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

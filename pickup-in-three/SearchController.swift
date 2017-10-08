5f0b8540e7781d455c97425811a3950f8ab933ce
ey1031 <eyudeveloper@gmail.com>
2017-10-08 01:19:38 -0700

    display keywords under lines and fixing search

--- pickup-in-three/SearchController.swift
+++ pickup-in-three/SearchController.swift
@@ -19,14 +19,20 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
     var searchActive : Bool = false
     var dataKeys: [[String]] = []
     var dataStrings: [String] = []
-    var data = ["San Francisco","New York","San Jose","Chicago","Los Angeles","Austin", "Seattle"]
     var filtered: [String] = []
+    var filteredKey: [[String]] = []
+    var completedLoading = false;
     
     override func viewDidLoad() {
         super.viewDidLoad();
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
         ref = FIRDatabase.database().reference();
         ref.child("lines").observeSingleEvent(of: .value, with: { (snapshot) in
-            
+            let totalCount = snapshot.childrenCount
             for line in snapshot.children.allObjects as! [FIRDataSnapshot] {
                 
                 // this should be changed to a nicer iterator
@@ -37,15 +43,15 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
                         keyArray.append((lineasdf as AnyObject).value);
                     }
                     self.dataKeys.append(keyArray);
-                    self.tableView.reloadData()
+                    if (self.dataKeys.count == Int(totalCount)) {
+                        self.tableView.reloadData();
+                        self.completedLoading = true
+                    }
                 });
 
             }
         });
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchBar.delegate = self
+    
     }
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
@@ -67,15 +73,13 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
 
         filtered.removeAll()
-//        for dataPoint in data {
-//            if ((dataPoint.lowercased().range(of: searchText.lowercased())) != nil) {
-//                filtered.append(dataPoint);
-//            }
-//        }
         for (index, dataPoint) in dataKeys.enumerated() {
             keywordIterator: for keyword in dataPoint {
                 if ((keyword.lowercased().range(of: searchText.lowercased())) != nil) {
+                    print(dataStrings[index])
+                    print(dataKeys[index])
                     filtered.append(dataStrings[index])
+                    filteredKey.append(dataKeys[index])
                     break keywordIterator
                 }
             }
@@ -83,8 +87,6 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
 
         if (searchText == "") {
             searchActive = false;
-            print("false")
-            print(filtered)
         }
         else {
             searchActive = true;
@@ -92,28 +94,36 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
         self.tableView.reloadData()
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1;
-    }
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 0;
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (searchActive) {
             return filtered.count;
         }
-//        return data.count;
         return dataStrings.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! UITableViewCell;
+        let cell : SearchCell? = self.tableView.dequeueReusableCell(withIdentifier: "Cell") as? SearchCell
+        
         if (searchActive) {
-            cell.textLabel?.text = filtered[indexPath.row];
+            cell?.lineText?.text = filtered[indexPath.row];
+            cell?.word1?.text = filteredKey[indexPath.row][0];
+            cell?.word2?.text = filteredKey[indexPath.row][1];
+            cell?.word3?.text = filteredKey[indexPath.row][2];
+        }
+        else if (completedLoading) {
+            cell?.lineText?.text = dataStrings[indexPath.row];
+            cell?.word1?.text = dataKeys[indexPath.row][0];
+            cell?.word2?.text = dataKeys[indexPath.row][1];
+            cell?.word3?.text = dataKeys[indexPath.row][2];
         }
         else {
-            cell.textLabel?.text = dataStrings[indexPath.row];
         }
         
-        return cell;
+        return cell!;
     }
     
     override func didReceiveMemoryWarning() {
@@ -123,3 +133,9 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
 }
 
+class SearchCell: UITableViewCell {
+    @IBOutlet var lineText: UILabel!
+    @IBOutlet var word1: UILabel!
+    @IBOutlet var word2: UILabel!
+    @IBOutlet var word3: UILabel!
+}
